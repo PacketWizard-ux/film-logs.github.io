538954c339210d5d4c9340bfe9803dc1378912ab
PacketWizard-ux <vanitpatel9888@gmail.com>
2025-11-28 01:05:34 +0530

    Update vite.config.js

--- vite.config.js
+++ vite.config.js
@@ -4,4 +4,6 @@ import react from '@vitejs/plugin-react'
 // https://vite.dev/config/
 export default defineConfig({
   plugins: [react()],
+  import { defineConfig } from 'vite'
+  base: '/film-logs.github.io/'
 })
